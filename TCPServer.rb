4487920856a76e251de20cd18dedb16acec8baf9
Max <nightmare40000@gmail.com>
2013-11-14 15:49:19 +0300

    TCPServer modified

--- TCPServer.rb
+++ TCPServer.rb
@@ -1,17 +1,45 @@
 require 'socket'
-TCPServer.open('localhost', 3000){ |server|
-    if (session = server.accept)
-        session.print "Welcome to server\r\nEnter \"exit\" to shutdown server.\r\n"
-        loop{
-            cmd, arg = session.gets.chomp.split
-            case cmd
-                when "exit"
-                    session.close
-                    break
-                else
-	    	    print cmd + "\n"		
-                    session.print cmd + "\n"
-            end
-        }
+
+READ_CHUNK = 1024
+LOCAL_PORT = 3000
+include Socket::Constants
+socket = Socket.new(AF_INET, SOCK_STREAM)
+addr = Socket.pack_sockaddr_in(LOCAL_PORT, '0.0.0.0')
+socket.bind(addr)
+socket.listen(5)
+socket.setsockopt(:SOCKET, :REUSEADDR, true)
+
+puts "Server is listening on port = #{LOCAL_PORT}"
+
+loop do 
+    begin
+    connection, addr_info = socket.accept
+    data_buffer = ""
+    
+    connection.write("You've been connected to server. Enter \"exit\" to disconnect\nData to send = ")		    
+
+    loop do
+        begin
+	    connection.read_nonblock(READ_CHUNK, data_buffer)
+ 	    
+	    if data_buffer.include? "exit"
+   			puts "Connection closed"
+			connection.close
+		      	break
+	    end
+	    connection.write("Data sended =  #{data_buffer}\nData to send = ")
+            puts "Data recieved = #{data_buffer}"
+
+        rescue Errno::EAGAIN => e
+            IO.select([connection])         
+            retry
+        rescue EOFError
+            break
+        end
     end
-}
+
+    rescue Exception, Interupt
+	puts "\nServer shutdown"
+	break;
+    end
+end
